bcd3a0ea4d26c2ab5691c0b6ec1c12f37e305dc1
heka1024 <heka1024@gmail.com>
2020-11-23 21:35:27 +0900

    add token

--- src/main/scala/example/Parsers.scala
+++ src/main/scala/example/Parsers.scala
@@ -2,6 +2,22 @@ package example
 
 import cats._, cats.data._, cats.implicits._
 
+case class Token(start: Int, mid: Int, end: Int) {
+    def toChar: Char = {
+      this.start * 21 * 28 + this.mid * 28 + this.end + 44032
+    }.toChar
+}
+
+object Token {
+  def apply(c: Char): Token = {
+    new Token(
+      (c.toInt - 44032) / (21 * 28),
+      { (c.toInt - 44032) % (21 * 28) } / 28,
+      { (c.toInt - 44032) % (21 * 28) } % 28
+    )
+  }
+}
+
 object Parsers {
   val firsts = "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ"
   val seconds = "ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ"
@@ -10,4 +26,6 @@ object Parsers {
   "가나다라마바사아자차"
     .map { _.toLong - 44032 }
     .map { _ / (21 * 28) }
+
+  def char2Token
 }
